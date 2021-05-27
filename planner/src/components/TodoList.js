c475ffc2130f9435bbc24c6cbb01af2b076b4d30
Indiana Lora <indiana@ensaludybelleza.com>
2021-05-27 11:08:33 -0400

    implemented usseEffect in todoList

--- planner/src/components/TodoList.js
+++ planner/src/components/TodoList.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+//espredd operator ->investigar
+//make a counter with setTimeout
 function TodoList() {
   const itemsArray = [
     {
       id: 1,
       item: "Leer",
+      hey: "sasa",
     },
     {
       id: 2,
@@ -14,10 +16,13 @@ function TodoList() {
   const [list, setList] = useState(itemsArray);
 
   const removeItem = (id) => {
-    let newList = list.filter((itemList) => itemList.id !== id);
-
+    let newList = list.filter((itemSelected) => itemSelected.id !== id);
     setList(newList);
   };
+
+  useEffect(()=>{
+    document.title='todoList'
+  })
 
   return (
     <div>
